280736514967ce80c1e11f2ecaef2cc277b5c87b
Renier <rero.reyes.up@phinmaed.com>
2023-12-04 10:05:48 +0800

    MainActivity.kt preview

--- app/src/main/java/com/reyes/renier/block2/p1/quiz/MainActivity.kt
+++ app/src/main/java/com/reyes/renier/block2/p1/quiz/MainActivity.kt
@@ -41,6 +41,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     RenierReyesBlock2QuizTheme {
-        Greeting("YO!")
+        Greeting("test1247862354")
     }
 }
